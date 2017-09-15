7b72091693424774c31f6df29025c1dfa441021b
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-09-15 06:48:49 +0300

    xzreader.c: implemented open, reopen, and free

---
+++ xzreader.c
@@ -0,0 +1,152 @@
+// Copyright (c) 2017 Alexey Tourbin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <stdio.h> // sys_errlist
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+#include <lzma.h>
+#include "reada.h"
+#include "xzreader.h"
+
+// A thread-safe strerror(3) replacement.
+static const char *xstrerror(int errnum)
+{
+    // Some of the great minds say that sys_errlist is deprecated.
+    // Well, at least it's thread-safe, and it does not deadlock.
+    if (errnum > 0 && errnum < sys_nerr)
+	return sys_errlist[errnum];
+    return "Unknown error";
+}
+
+// The list of possible errors during decoding is based on
+// egrep 'case.*ERROR:|msg =' doc/examples/02_decompress.c
+static const char *lzma_strerror(lzma_ret ret)
+{
+    switch (ret) {
+    case LZMA_MEM_ERROR:
+	return "Memory allocation failed";
+    case LZMA_OPTIONS_ERROR:
+	return "Unsupported decompressor flags";
+    case LZMA_FORMAT_ERROR:
+	return "The input is not in the .xz format";
+    case LZMA_DATA_ERROR:
+	return "Compressed file is corrupt";
+    case LZMA_BUF_ERROR:
+	return "Compressed file is truncated or otherwise corrupt";
+    default:
+	break;
+    }
+    return "Unknown error, possibly a bug";
+}
+
+// Helpers to fill err[2] arg.
+#define ERRNO(func) err[0] = func, err[1] = xstrerror(errno)
+#define ERRXZ(func, ret) err[0] = func, err[1] = lzma_strerror(ret)
+#define ERRSTR(str) err[0] = __func__, err[1] = str
+
+// Start decoding at the beginning of a stream.
+static int xzreader_begin(lzma_stream *lzma, struct fda *fda, const char *err[2])
+{
+    unsigned char buf[LZMA_STREAM_HEADER_SIZE];
+    ssize_t n = reada(fda, &buf, sizeof buf);
+    if (n < 0)
+	return ERRNO("read"), -1;
+    if (n == 0)
+	return 0;
+    if (n < sizeof buf)
+	return ERRSTR("input too small"), -1;
+
+    lzma->next_in = buf, lzma->avail_in = sizeof buf;
+    lzma->next_out = NULL, lzma->avail_out = 0;
+
+    lzma_ret zret = lzma_code(lzma, LZMA_RUN);
+    if (zret != LZMA_OK)
+	return ERRXZ("lzma_code", zret), -1;
+
+    assert(lzma->avail_in == 0);
+
+    return 1;
+}
+
+struct xzreader {
+    struct fda *fda;
+    lzma_stream lzma;
+    bool eof;
+};
+
+// 80M should be enough to decode 'xz -9' output.  The limit once was
+// in use and was specified in the pre-5.0 xz(1) manpage.  The limit
+// exemplifies the Pareto principle.
+#define MEMLIMIT (80<<20)
+
+int xzreader_open(struct xzreader **zp, struct fda *fda, const char *err[2])
+{
+    lzma_stream lzma = LZMA_STREAM_INIT;
+    lzma_ret zret = lzma_stream_decoder(&lzma, MEMLIMIT, 0);
+    if (zret != LZMA_OK)
+	return ERRXZ("lzma_stream_decoder", zret), -1;
+
+    int rc = xzreader_begin(&lzma, fda, err);
+    if (rc <= 0)
+	return lzma_end(&lzma), rc;
+
+    struct xzreader *z = malloc(sizeof *z);
+    if (!z)
+	return ERRNO("malloc"), lzma_end(&lzma), -1;
+
+    z->fda = fda;
+    z->lzma = lzma;
+    z->eof = false;
+
+    *zp = z;
+    return 1;
+}
+
+int xzreader_reopen(struct xzreader *z, struct fda *fda, const char *err[2])
+{
+    if (fda)
+	z->fda = fda;
+
+    // The stream can be reused only upon successful decoding.
+    if (!z->eof) {
+	lzma_end(&z->lzma);
+	z->lzma = (lzma_stream) LZMA_STREAM_INIT;
+
+	lzma_ret zret = lzma_stream_decoder(&z->lzma, MEMLIMIT, 0);
+	if (zret != LZMA_OK)
+	    return ERRXZ("lzma_stream_decoder", zret), -1;
+    }
+
+    z->eof = false;
+
+    return xzreader_begin(&z->lzma, z->fda, err);
+}
+
+void xzreader_free(struct xzreader *z)
+{
+    if (!z)
+	return;
+    lzma_end(&z->lzma);
+    free(z);
+}
+
+// ex:set ts=8 sts=4 sw=4 noet:
---
+++ xzreader.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2017 Alexey Tourbin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef XZREADER_H
+#define XZREADER_H
+
+#include <sys/types.h> // ssize_t
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct fda; // reada.h
+struct xzreader;
+
+// Returns 1 on success, 0 on EOF, -1 on error.  On success, the Reader
+// handle is returned via zp.  Information about the error is returned via
+// the err[2] parameter: the first string is typically the function name,
+// and the second is the string which describes the error.  Both strings
+// normally come from the read-only data section.
+int xzreader_open(struct xzreader **zp, struct fda *fda, const char *err[2])
+		  __attribute__((nonnull));
+
+// The open/read functions process only one XZ frame (also called a stream),
+// and do not read past the end of that frame.  Multiple frames can be
+// concatenated, but then frame boundaries can be meaningful.  It is possible
+// to reuse the Reader for reading another frame.
+// When fda is NULL, this function will reuse the previous file descriptor;
+// otherwise, the previous file descriptor will not be closed automatically.
+int xzreader_reopen(struct xzreader *z, struct fda *fda, const char *err[2])
+		    __attribute__((nonnull(1, 3)));
+
+// Doesn't close its file descriptor.
+void xzreader_free(struct xzreader *z);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
